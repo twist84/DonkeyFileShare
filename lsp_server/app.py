6333dd4ce2845e5ffbf640d4f8fecc296261839b
theTwister <6237734+twist84@users.noreply.github.com>
2024-01-22 19:25:13 +0000

    Updated `lsp_server`, upload now supported

--- lsp_server/app.py
+++ lsp_server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, send_file, request, abort
+from werkzeug.utils import secure_filename
 import logging
 import sys
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/<path:path>', methods=['GET'])
 def handle_get(path):
-    file_path = '.' + request.path
+    file_path = './www/' + request.method + request.path
     try:
         return send_file(file_path, mimetype='application/octet-stream')
     except FileNotFoundError:
@@ -14,10 +15,20 @@ def handle_get(path):
         abort(404)
 
 @app.route('/upload_server/upload.ashx', methods=['POST'])
-def handle_post():
-    post_data = request.data
-    logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\n",str(request.path), str(request.headers))
-    return '', 200
+def upload_file():
+    if 'upload' not in request.files:
+        return 'No file part', 400
+
+    file = request.files['upload']
+    if file.filename == '':
+        return 'No selected file', 404
+
+    filename = secure_filename(file.filename)
+    content_type = request.headers['Content-Type']
+
+    file.save('./www/' + request.method + '/uploads/' + filename)
+
+    return f'File {filename} uploaded with content type {content_type}', 200
 
 if __name__ == '__main__':
         app.run()
